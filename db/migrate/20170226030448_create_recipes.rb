0cf0b67bdfa94b92ca0bfd72255c44acc58b9b75
BenShoeman <ben.shoeman@outlook.com>
2017-02-25 21:00:47 -0700

    Fixed messed up migration files and used db:migrate

--- db/migrate/20170226030448_create_recipes.rb
+++ db/migrate/20170226030448_create_recipes.rb
@@ -2,9 +2,9 @@ class CreateRecipes < ActiveRecord::Migration[5.0]
   def change
     create_table :recipes do |t|
       t.string :name
-      t.int :servings
-      t.int :prep_time
-      t.int :cook_time
+      t.integer :servings
+      t.integer :prep_time
+      t.integer :cook_time
       t.string :image_url
 
       t.timestamps
--- db/migrate/20170226030550_create_ingredients.rb
+++ db/migrate/20170226030550_create_ingredients.rb
@@ -2,7 +2,7 @@ class CreateIngredients < ActiveRecord::Migration[5.0]
   def change
     create_table :ingredients do |t|
       t.string :name
-      t.int :times_searched
+      t.integer :times_searched
       t.references :recipe, foreign_key: true
 
       t.timestamps
