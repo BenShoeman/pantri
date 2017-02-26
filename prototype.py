ae7ddcaf3320c55df576d9aee3c731484a9a96e7
BenShoeman <ben.shoeman@outlook.com>
2017-02-26 14:57:38 -0700

    Removing .encode('utf-8') because this was not working as expected, and added time.sleep(5) for demo and also to not connect so fast

--- prototype.py
+++ prototype.py
@@ -4,18 +4,17 @@ import time
 import re
 import sys
 
-
 def recipe_spider(recipe_url):
 	source_coder = requests.get(recipe_url)
 	recipe_text = source_coder.text
 	soup = BeautifulSoup(recipe_text)
 	for meal in soup.findAll('h1',{'class': 'entry-title'}):
 		print("DISH: ")
-		print(meal.string).encode('utf-8')
+		print(meal.string)
 	for ingredients in soup.findAll('li',{'class': 'ingredient'}):
-		print(ingredients.string).encode('utf-8')
+		print(ingredients.string)
 	for instruction in soup.findAll('li',{'class': 'instruction'}):
-		print(instruction.string).encode('utf-8')
+		print(instruction.string)
 def link_spider():
 	url = 'http://www.everydaymaven.com/recipe-index-text-based/'
 	source_code = requests.get(url)
@@ -25,6 +24,9 @@ def link_spider():
 		traversable = link.get('href')
 		print(traversable)
 		recipe_spider(traversable)
+                # Adding time.sleep for five seconds so that we don't get denied
+                # access, also for demoing the project
+                time.sleep(5)
 link_spider()
 
 #def link_spider():
