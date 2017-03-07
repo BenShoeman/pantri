fb81e19ca181ba5b116eb21fe0926ceae1f3922b
BenShoeman <ben.shoeman@outlook.com>
2017-03-06 22:23:51 -0700

    Search is workinggit status!

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,10 +1,18 @@
 class Recipe < ApplicationRecord
-  has_many :ingredients
   has_many :recipe_ingredients
+  has_many :ingredients, :through => :recipe_ingredients
 
   def self.search(search)
     Recipe.joins(:recipe_ingredients).joins(:ingredients).
-      where("ingredients.name LIKE ?", "%#{search}%")
-    # where("ingredients LIKE ?", "%#{search}%")
+      where("ingredients.name ILIKE ?", "%#{search}%").uniq
   end
+
+  def self.none()
+    where("1 = 0")
+  end
+
+  # def self.getIngredients(search)
+  #   Recipe.joins(:recipe_ingredients).joins(:ingredients).
+  #     where("ingredients.name ILIKE ?", "%#{search}%")
+  # end
 end
