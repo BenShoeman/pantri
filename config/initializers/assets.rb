a3bea71b064c106c64250a852509231f6946fa4f
Nabil Ettachfini <Nabillionaire@rgnt1-31-35-dhcp.int.colorado.edu>
2017-03-19 16:47:35 -0600

    Fixing problem that model-specific SCSS files would be loaded in all views, not the specific view

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,7 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( recipes.scss )
+Rails.application.config.assets.precompile += %w( ingredients.scss )
+Rails.application.config.assets.precompile += %w( recipe_ingredients.scss )
+Rails.application.config.assets.precompile += %w( users.scss )
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
   resources :recipe_ingredients
   resources :ingredients
   resources :recipes
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get 'recipes/index' => 'recipes#index'
