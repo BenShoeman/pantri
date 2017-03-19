b2db972f9f7ab34f6843354ee4873a600857f1fa
BenShoeman <ben.shoeman@outlook.com>
2017-03-19 03:50:32 -0600

    Fixing broken migration. Rails didn't like adding ID since it automatically adds ID to every table so I removed the t.integer :id line

--- db/migrate/20170308030310_create_users.rb
+++ db/migrate/20170308030310_create_users.rb
@@ -1,7 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.integer :id
       t.string :name
       t.string :email
       t.string :password
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170307024528) do
+ActiveRecord::Schema.define(version: 20170308030310) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,14 @@ ActiveRecord::Schema.define(version: 20170307024528) do
     t.string   "image_url",  limit: 2083
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "recipe_ingredients", "ingredients"
