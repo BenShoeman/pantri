a5c8b8b1ab2bcdb6df93a477f6906638dad03758
Kingston Rainsworth <hovu0566@colorado.edu>
2017-03-20 14:43:48 -0600

    fix tail part

--- prototype2.py
+++ prototype2.py
@@ -12,7 +12,7 @@ def link_spider(url):
     for ul in soup.findAll('ul',{'class' : 'm-PromoList o-Capsule__m-PromoList'}):
         for li in ul.findAll('li',{'class' : 'm-PromoList__a-ListItem'}):
             for a in li.findAll('a'):
-                time.sleep(5)
+                time.sleep(2)
                 print(a.get('href'))
                 recipe_url = a.get('href')
                 recipe_spider(recipe_url)
@@ -27,11 +27,10 @@ def recipe_spider(urls):
             startcheck = json_string.find("{")
             json_stringp = json_string[startcheck:]
             json_stringp = json_stringp.replace("</script>","")
-            #print(json_stringp)
             json_object = json.loads(json_stringp)
             print json_object['name']
-    except requests.exceptions.TooManyRedirects as e:
-        print "Error: Too many redirects."
+    except (requests.exceptions.TooManyRedirects,requests.exceptions.MissingSchema,KeyError) as e:
+        print "Error"
 
-link_spider('http://www.foodnetwork.com/recipes/a-z')
-#recipe_spider('http://www.foodnetwork.com/recipes/caesar-salad-recipe')
+#link_spider('http://www.foodnetwork.com/recipes/a-z')
+#recipe_spider('http://www.foodnetwork.com/grilling/grilling-central-burgers-and-hot-dogs')
