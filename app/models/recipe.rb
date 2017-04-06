b4a482e5a6f28cebe1e6fda51a3af6d040850e98
Courtney Solano <courtsolano@gmail.com>
2017-04-06 14:19:24 -0600

    Created relationships and table for save and user

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,7 @@
 class Recipe < ApplicationRecord
   has_many :recipe_ingredients
   has_many :ingredients, :through => :recipe_ingredients
+  has_and_belongs_to_many :users
 
   def self.search(search)
     params = search.split(/,/) # Split by comma
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@
 
 class User < ActiveRecord::Base
 
+  has_and_belongs_to_many :recipes
   has_secure_password
 
 end
