4f38983b124451a8dc35b1d2382f0e5169a13873
BenShoeman <ben.shoeman@outlook.com>
2017-04-11 20:56:01 -0600

    Fixing another missing end statement

--- app/controllers/user_ingredients_controller.rb
+++ app/controllers/user_ingredients_controller.rb
@@ -1,27 +1,28 @@
 # Nabil Ettachfini and Alexander Hamlet worked on this
 class User_ingredients_controller < ApplicationController
-    def new
-        @user_ings = userIngController.new
-    end
+  def new
+    @user_ings = userIngController.new
+  end
 
-def list
+  def list
     @user_ings = user_ingredients.all # this should make a list of all ingredients
   end
 
-def show
-  @user_ingr = user_ingr.find(params(:id))
-end
+  def show
+    @user_ingr = user_ingr.find(params(:id))
+  end
 
-def create
-  @user_ingr = user_ingr.new(ingredients_params)
+  def create
+    @user_ingr = user_ingr.new(ingredients_params)
     if @user_ingr.save
         redirect_to:action => 'list'
     else
       @subjects = Subject.all
       render :action => 'edit'
     end
-end
+  end
 
-def user_recipe_ingrediants
-  params.required(:ingredients).permit(:ingredients)
+  def user_recipe_ingrediants
+    params.required(:ingredients).permit(:ingredients)
+  end
 end
