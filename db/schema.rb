a7c5d32c97bd11b8a1c0b2c1d4fae387461d56d4
BenShoeman <ben.shoeman@outlook.com>
2017-04-23 02:50:38 -0600

    Design changes and small bug fixes with loading recipes when user not logged in

--- db/schema.rb
+++ db/schema.rb
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 20170414001731) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
+    t.string   "password"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
