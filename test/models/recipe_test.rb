c7d4af4f5b8f48f9f7c451abd88347219ef614c1
BenShoeman <ben.shoeman@outlook.com>
2017-04-30 02:43:44 -0600

    Fixing broken test cases

--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -1,23 +1,23 @@
 require 'test_helper'
 
 class RecipeTest < ActiveSupport::TestCase
-  test "should return 1 recipe with search('ingr1')" do
-    recipes = Recipe.search('ingr1')
-    assert recipes.size == 1
+  test "should return 1 recipe with search('ingr1',1)" do
+    recipes = Recipe.search('ingr1',1)
+    assert recipes.length == 1
   end
 
-  test "should return Recipe 1 with search('ingr1')" do
-    recipes = Recipe.search('ingr1')
+  test "should return Recipe 1 with search('ingr1',1)" do
+    recipes = Recipe.search('ingr1',1)
     assert recipes[0].name == "Recipe 1"
   end
 
-  test "should return 2 recipes with search('ingr3')" do
-    recipes = Recipe.search('ingr3')
-    assert recipes.size == 2
+  test "should return 2 recipes with search('ingr3',1)" do
+    recipes = Recipe.search('ingr3',1)
+    assert recipes.length == 2
   end
 
-  test "should return [Recipe 1, Recipe 2] with search('ingr3')" do
-    recipes = Recipe.search('ingr3')
+  test "should return [Recipe 1, Recipe 2] with search('ingr3',1)" do
+    recipes = Recipe.search('ingr3',1)
     if recipes[0].name == "Recipe 1"
       assert recipes[0].name == "Recipe 1" and recipes[1].name == "Recipe 2"
     else
@@ -25,24 +25,24 @@ class RecipeTest < ActiveSupport::TestCase
     end
   end
 
-  test "should return 2 recipes with search('ingr1,ingr2')" do
-    recipes = Recipe.search('ingr1,ingr2')
-    assert recipes.size == 2
+  test "should return 2 recipes with search('ingr1,ingr2',1)" do
+    recipes = Recipe.search('ingr1,ingr2',1)
+    assert recipes.length == 2
   end
 
-  test "should return 2 recipes with search('ingr1, ingr2, ingr3 ')" do
-    recipes = Recipe.search('ingr1, ingr2, ingr3 ')
-    assert recipes.size == 2
+  test "should return 2 recipes with search('ingr1, ingr2, ingr3 ',1)" do
+    recipes = Recipe.search('ingr1, ingr2, ingr3 ',1)
+    assert recipes.length == 2
   end
 
-  test "should return nothing with search('ingr4')" do
-    recipes = Recipe.search('ingr4')
-    assert recipes.size == 0
+  test "should return nothing with search('ingr4',1)" do
+    recipes = Recipe.search('ingr4',1)
+    assert recipes.length == 0
   end
 
-  test "should return 1 recipe with search('ingr2, ingr4')" do
-    recipes = Recipe.search('ingr2, ingr4')
-    assert recipes.size == 1
+  test "should return 1 recipe with search('ingr2, ingr4',1)" do
+    recipes = Recipe.search('ingr2, ingr4',1)
+    assert recipes.length == 1
   end
 
   test "should return Recipe 2 with search('ingr2, ingr4')" do
